2f8ae3c655860b1156149aa14a267d3c96f0a052
Nami Fujisaki <nfujisak@c1r3s4.42tokyo.jp>
2024-04-16 14:11:54 +0900

    header

---
+++ include/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfujisak <nfujisak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 14:04:38 by nfujisak          #+#    #+#             */
+/*   Updated: 2024/04/16 14:04:53 by nfujisak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+typedef struct s_list // i dont understand this yet
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+#endif
