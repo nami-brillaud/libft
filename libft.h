48524841b01437798e81918726c9a3bc04f6ab34
Nami Fujisaki <nfujisak@c1r4s6.42tokyo.jp>
2024-04-17 19:19:39 +0900

    isdigit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nfujisak <nfujisak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 14:04:38 by nfujisak          #+#    #+#             */
-/*   Updated: 2024/04/17 17:24:57 by nfujisak         ###   ########.fr       */
+/*   Updated: 2024/04/17 19:19:20 by nfujisak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *ptr, int x, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 typedef struct s_list
 {
