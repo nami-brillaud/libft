a2d114feb7f3a8212cf4da0b73e3d0f25b5480e6
Nami Fujisaki <nfujisak@c1r2s4.42tokyo.jp>
2024-04-26 16:12:04 +0900

    fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: nfujisak <nfujisak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:29:39 by nfujisak          #+#    #+#             */
-/*   Updated: 2024/04/19 17:53:10 by nfujisak         ###   ########.fr       */
+/*   Updated: 2024/04/26 16:11:58 by nfujisak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,29 @@ static size_t	strlen(const char *s)
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dstlen;
-	size_t	srclen;
-	size_t	maxcopy;
-	size_t	i;
+	size_t		dstlen;
+	size_t		totallen;
+	const char	*src_ptr;
+	size_t		i;
 
-	if (dst == NULL)
-		return (strlen(src));
-	dstlen = strlen(dst);
-	srclen = strlen(src);
-	if (dstlen >= dstsize)
-		return (dstsize + srclen);
-	maxcopy = dstsize - (dstlen + 1);
-	i = 0;
-	while (i < maxcopy && *src)
+	if ((!dst || !src) & !dstsize)
+		return (0);
+	src_ptr = src;
+	dstlen = 0;
+	while (dst[dstlen] && dstlen < dstsize)
+		dstlen++;
+	if (dstlen < dstsize)
+		totallen = dstlen + strlen(src_ptr);
+	else
+		return (dstsize + strlen(src_ptr));
+	i = dstlen;
+	while (i < dstsize - 1 && *src_ptr)
 	{
-		dst[dstlen + i] = src[i];
+		dst[i] = *src_ptr++;
 		i++;
 	}
-	dst[dstlen + i] = '\0';
-	return (dstlen + srclen);
+	dst[i] = '\0';
+	return (totallen);
 }
 
 // #include <stdio.h>
@@ -52,8 +55,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 // 	char src[2] = "";
 // 	char dst[50] = "";
 // 	printf("The source is %s, the dest is %s\n", src, dst);
-// 	size_t result = ft_strlcat(dst, src, 0);
+// 	size_t result = ft_strlcat(dst, src, -1);
 // 	printf("Now the source is %s, the dest is %s, the result is %zu\n", 
-// src, dst, result);
+// 	src, dst, result);
 // 	return (0);
 // }
