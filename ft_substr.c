a0fb1d8d76060dd8d8a30fdf4afe06692a84f631
Nami Fujisaki <nfujisak@c1r2s4.42tokyo.jp>
2024-04-26 17:36:50 +0900

    1st attemp substr

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfujisak <nfujisak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 16:55:23 by nfujisak          #+#    #+#             */
+/*   Updated: 2024/04/26 17:36:39 by nfujisak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	slen;
+
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (NULL);
+	if (start + len > slen)
+		len = slen - start;
+	substr = malloc (sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char test[] = "Hey there";
+// 	printf("%s\n", ft_substr(test, 4, 6));
+// 	return (0);
+// }
